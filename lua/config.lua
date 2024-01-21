b0f0f07603a58702aa30426c72c963579170e504
Nullctipus <nullctipus@gmail.com>
2024-01-21 12:04:37 -0600

    up

--- lua/config.lua
+++ lua/config.lua
@@ -3,5 +3,8 @@ local opts = { noremap = true, silent = true }
 
 map('n', '<leader>n', '<Cmd>Neotree<CR>',opts)
 map('n', '<leader>ca', '<Cmd>lua vim.lsp.buf.code_action()<CR>',opts)
+map('n', '<leader>bn', '<Cmd>bnext<CR>',opts)
+map('n', '<leader>bp', '<Cmd>bprev<CR>',opts)
 
 vim.cmd.colorscheme "catppuccin"
+vim.api.nvim_set_option("clipboard","unnamedplus");
