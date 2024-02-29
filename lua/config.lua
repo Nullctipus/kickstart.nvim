87c5b8534293bac67736728dba2fbdf6d4441fec
Nullctipus <nullctipus@gmail.com>
2024-02-28 18:03:49 -0600

    update keybinds

--- lua/config.lua
+++ lua/config.lua
@@ -1,13 +1,11 @@
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<leader>n', '<Cmd>Neotree<CR>', { noremap = true, silent = true, desc = '[N]eotree' })
+vim.keymap.set('n', '<leader>ca', '<Cmd>lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true, desc = '[C]ode [A]ction' })
+vim.keymap.set('n', '<leader>bn', '<Cmd>BufferNext<CR>', { noremap = true, silent = true, desc = '[B]uffer [N]ext' })
+vim.keymap.set('n', '<leader>bp', '<Cmd>BufferPrevious<CR>', { noremap = true, silent = true, desc = '[B]uffer [P]revious' })
+vim.keymap.set('n', '<leader>bc', '<Cmd>BufferClose<CR>', { noremap = true, silent = true, desc = '[B]uffer [C]lose' })
+vim.keymap.set('n', '<leader>f', '<Cmd>Format<CR>', { noremap = true, silent = true, desc = '[F]ormat' })
 
-map('n', '<leader>n', '<Cmd>Neotree<CR>',opts)
-map('n', '<leader>ca', '<Cmd>lua vim.lsp.buf.code_action()<CR>',opts)
-map('n', '<leader>bn', '<Cmd>bnext<CR>',opts)
-map('n', '<leader>bp', '<Cmd>bprev<CR>',opts)
-map('n', '<leader>f', '<Cmd>Format<CR>',opts)
-
-vim.cmd.colorscheme "gruvbox";
-vim.opt.foldmethod = "indent";
-vim.opt.foldenable = false;
-vim.opt.colorcolumn = "81";
+vim.cmd.colorscheme 'gruvbox'
+vim.opt.foldmethod = 'indent'
+vim.opt.foldenable = false
+vim.opt.colorcolumn = '81'
