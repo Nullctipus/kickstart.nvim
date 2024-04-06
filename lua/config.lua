63d1cee21711a2b1f56cbeaa489e836358935204
Nullctipus <nullctipus@gmail.com>
2024-04-06 15:23:02 -0500

    remove copilot

--- lua/config.lua
+++ lua/config.lua
@@ -5,11 +5,10 @@ vim.keymap.set('n', '<leader>bp', '<Cmd>BufferPrevious<CR>', { noremap = true, s
 vim.keymap.set('n', '<leader>bc', '<Cmd>BufferClose<CR>', { noremap = true, silent = true, desc = '[B]uffer [C]lose' })
 vim.keymap.set('n', '<leader>f', '<Cmd>Format<CR>', { noremap = true, silent = true, desc = '[F]ormat' })
 
-vim.g['copilot_no_tab_map'] = true
-vim.g['copilot_assume_mapped'] = true
-vim.keymap.set('i', '<C-/>', 'copilot#Accept("<CR>")', { expr = true, replace_keycodes = false, noremap = true, silent = true, desc = 'Accept' })
-
 vim.cmd.colorscheme 'gruvbox'
 vim.opt.foldmethod = 'indent'
 vim.opt.foldenable = false
 vim.opt.colorcolumn = '81'
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
